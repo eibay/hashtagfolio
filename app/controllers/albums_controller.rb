1812435cada257584102993cf0047d11e7320987
Hugh Middleton <hughfmiddleton@gmail.com>
2015-08-24 13:13:50 +1000

    load user's recent posts

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,12 @@
 class AlbumsController < ApplicationController
   def index
+    @albums = Album.all
+  end
 
+  def show
+    @album = Album.find params[:id]
+    @user = @album.user
+    client = Instagram.client access_token: @user.instagram_access_token
+    @photos = client.user_recent_media.map { |photo| photo.images.standard_resolution }
   end
 end
